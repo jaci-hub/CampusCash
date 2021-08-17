8c7053e1a3ed7d59dfca9c2faa98a65643e6e20a
jaci-hub <jacintoquimua7@gmail.com>
2021-08-17 14:39:48 -0600

    getTransactionFee.h is DONE!!!

--- maincc/getTransactionFee.h
+++ maincc/getTransactionFee.h
@@ -4,8 +4,28 @@
 #include "feesManagement.h"
 using namespace std;
 
+int qstateget_transactionFee;
 double amount_toSend;
 
 double get_transactionFee() { 
-    return .02 * amount_toSend;
+	//Stablishing the connection to mysql database
+	MYSQL* conn;
+	MYSQL_ROW row;
+	MYSQL_RES* res;
+	conn = mysql_init(0);
+
+	conn = mysql_real_connect(conn, "localhost", "root", "ReinoDaMatamba3", "allstudentdata", 3306, NULL, 0);
+
+	if (conn) {
+		string queryGettingTransactionFee = "SELECT percentageFee FROM feesTable WHERE feeName = 'TransactionFee'";
+		const char* qGettingTransactionFee = queryGettingTransactionFee.c_str();
+		qstateget_transactionFee = mysql_query(conn, qGettingTransactionFee);
+		if (!qstateget_transactionFee) {
+			res = mysql_store_result(conn);
+			row = mysql_fetch_row(res);
+			return amount_toSend * stod(row[0])/100.00;
+		}
+		else cout << "Query failed: " << mysql_error(conn) << "\n";
+	}
+	else puts("Connection to DataBase has failed");
 }
