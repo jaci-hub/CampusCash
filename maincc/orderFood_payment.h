42d5ef1940099d161c5d422639e044bd09152aa2
jaci-hub <jacintoquimua7@gmail.com>
2021-08-30 17:51:08 -0600

    only missing payment method and place order

--- maincc/orderFood_payment.h
+++ maincc/orderFood_payment.h
@@ -24,8 +24,40 @@ void orderFood_payment() {
         cout << "* Delivery Fee: $" << orderPaymentFee() << "\n";
         cout << "** TOTAL: $" << myOrder.get_foodOrderTotal() + orderPaymentFee() << "\n";
 
-        //paymentMethod(); ???
+        //payment Method AVAILABLE AT SPECIFIED TIMES
+        paymentMethod:
+        cout << "* Select payment method\n";
+        cout << "1- Meals\n";
+        cout << "2- Coyote Cash\n";
+        cout << "Please, enter an option: ";
+        string metodoDePagamento;
+        cin >> metodoDePagamento;
+        if (metodoDePagamento == "1")
+            myOrder.paymentMethod = "Meals";
+        else if (metodoDePagamento == "2")
+            myOrder.paymentMethod = "Coyote Cash";
+        else goto paymentMethod;
+
+        cout << "* Select an option\n";
+        cout << "1- Place order\n";
+        cout << "2- Cancel order\n";
+        cout << "b- Back\n";
+        cout << "Please, enter an option: ";
+        string orderChoice;
+        cin >> orderChoice;
+        if (orderChoice == "1") {
+            //add to orders table
+
+            //display message
+        }
+        else if (orderChoice == "2")
+            goto orderFood_paymentEnd;
+        else if (orderChoice == "b")
+            goto paymentMethod;
     }
     else puts("Connection to DataBase has failed");
+
+orderFood_paymentEnd:
+    cout << "";
 }
 //*******
