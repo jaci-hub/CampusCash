53592fc172577c26e409b2d7a8b409f6322a19e6
jaci-hub <jacintoquimua7@gmail.com>
2021-09-02 10:51:51 -0600

    ask for send cash confirmation DONE!!!

--- maincc/send_cash.h
+++ maincc/send_cash.h
@@ -38,73 +38,90 @@ void send_cash() {
             send_cash();
         }
         else if (amount_toSend <= student1.get_balance()) {
-            //Taking care of sender
-            string queryEmail1 = "SELECT studentBalance FROM studentdatatable WHERE studentEmail = '" + student1.get_email() + "'";
-            const char* qEmail1 = queryEmail1.c_str();
-            qstateTransaction = mysql_query(conn, qEmail1);
-            if (!qstateTransaction) {
-                res = mysql_store_result(conn);
-                row = mysql_fetch_row(res);
-                string oldCash = row[0];
-                double newCash = stod(oldCash);
-                newCash -= amount_toSend + get_transactionFee();
+            //show total
+            cout << "Amount: $" << amount_toSend << "\n";
+            cout << "Transaction fee: $" << get_transactionFee() << "\n";
+            cout << "TOTAL: $" << amount_toSend + get_transactionFee() << "\n";
+            cout << "* Select an option\n";
+            cout << "1- Make transaction\n";
+            cout << "2- Cancel transaction\n";
+            cout << "Please, enter an option: ";
+            int transactionConfirmation;
+            cin >> transactionConfirmation;
+            if (transactionConfirmation == 1) {
+                //Taking care of sender
+                string queryEmail1 = "SELECT studentBalance FROM studentdatatable WHERE studentEmail = '" + student1.get_email() + "'";
+                const char* qEmail1 = queryEmail1.c_str();
+                qstateTransaction = mysql_query(conn, qEmail1);
+                if (!qstateTransaction) {
+                    res = mysql_store_result(conn);
+                    row = mysql_fetch_row(res);
+                    string oldCash = row[0];
+                    double newCash = stod(oldCash);
+                    newCash -= amount_toSend + get_transactionFee();
 
-                //Update senders balance in the classSender
-                student1.balance = newCash;
+                    //Update senders balance in the classSender
+                    student1.balance = newCash;
 
-                //Update senders balance in the DB
-                string newCashString = to_string(newCash);
-                string querynewCash1 = "UPDATE studentdatatable SET studentBalance = '" + newCashString + "' WHERE studentEmail = '" + student1.get_email() + "'";
-                const char* qnewCash1 = querynewCash1.c_str();
-                qstateTransaction = mysql_query(conn, qnewCash1);
+                    //Update senders balance in the DB
+                    string newCashString = to_string(newCash);
+                    string querynewCash1 = "UPDATE studentdatatable SET studentBalance = '" + newCashString + "' WHERE studentEmail = '" + student1.get_email() + "'";
+                    const char* qnewCash1 = querynewCash1.c_str();
+                    qstateTransaction = mysql_query(conn, qnewCash1);
+                    if (qstateTransaction)
+                        cout << "Query failed: " << mysql_error(conn) << "\n";
+                }
+                else cout << "Query failed: " << mysql_error(conn) << "\n";
+
+                //Taking care of receiver
+                string queryEmail2 = "SELECT studentBalance FROM studentdatatable WHERE studentEmail = '" + receiversEmail + "'";
+                const char* qEmail2 = queryEmail2.c_str();
+                qstateTransaction = mysql_query(conn, qEmail2);
+                if (!qstateTransaction) {
+                    res = mysql_store_result(conn);
+                    row = mysql_fetch_row(res);
+                    string oldCash = row[0];
+                    double newCash = stod(oldCash);
+                    newCash += amount_toSend;
+
+                    //Update receivers balance in the DB
+                    string newCashString = to_string(newCash);
+                    string querynewCash2 = "UPDATE studentdatatable SET studentBalance = '" + newCashString + "' WHERE studentEmail = '" + receiversEmail + "'";
+                    const char* qnewCash2 = querynewCash2.c_str();
+                    qstateTransaction = mysql_query(conn, qnewCash2);
+                    if (qstateTransaction)
+                        cout << "Query failed: " << mysql_error(conn) << "\n";
+                }
+                else cout << "Query failed: " << mysql_error(conn) << "\n";
+
+                //Create cashTransRecoTable if it doesnt exist yet
+                if (tableExists("cashTransRecoTable") == false) {
+                    string queryCriarTable = "CREATE TABLE cashTransRecoTable(transactionCount INT KEY AUTO_INCREMENT, senderEmail VARCHAR(255) NOT NULL, receiverEmail VARCHAR(255) NOT NULL, amountSent DOUBLE(5, 2) NOT NULL, feeApplied DOUBLE(5, 2), transDateTime VARCHAR(255) NOT NULL)";
+                    const char* qCriarTable = queryCriarTable.c_str();
+                    qstateTransaction = mysql_query(conn, qCriarTable);
+                    if (qstateTransaction)
+                        cout << "Query failed: " << mysql_error(conn) << "\n";
+                }
+
+                //Inserting values into cashTransRecoTable
+                string amount_toSendString = to_string(amount_toSend), get_transactionFeeString = to_string(get_transactionFee());
+                string queryEmail12amountSent = "INSERT INTO cashTransRecoTable (senderEmail, amountSent, receiverEmail, feeApplied, transDateTime) VALUES ('" + student1.get_email() + "', '" + amount_toSendString + "', '" + receiversEmail + "', '" + get_transactionFeeString + "', '" + getCurrentDateTime() + "')";
+                const char* qEmail12amountSent = queryEmail12amountSent.c_str();
+                qstateTransaction = mysql_query(conn, qEmail12amountSent);
                 if (qstateTransaction)
                     cout << "Query failed: " << mysql_error(conn) << "\n";
+
+                //Display transaction on the screen
+                cout << "*** Campus Cash (CC)***" << "\n";
+                cout << receiversName << " received $" << fixed << setprecision(2) << amount_toSend << "\n";
             }
-            else cout << "Query failed: " << mysql_error(conn) << "\n";
-
-            //Taking care of receiver
-            string queryEmail2 = "SELECT studentBalance FROM studentdatatable WHERE studentEmail = '" + receiversEmail + "'";
-            const char* qEmail2 = queryEmail2.c_str();
-            qstateTransaction = mysql_query(conn, qEmail2);
-            if (!qstateTransaction) {
-                res = mysql_store_result(conn);
-                row = mysql_fetch_row(res);
-                string oldCash = row[0];
-                double newCash = stod(oldCash);
-                newCash += amount_toSend;
-
-                //Update receivers balance in the DB
-                string newCashString = to_string(newCash);
-                string querynewCash2 = "UPDATE studentdatatable SET studentBalance = '" + newCashString + "' WHERE studentEmail = '" + receiversEmail + "'";
-                const char* qnewCash2 = querynewCash2.c_str();
-                qstateTransaction = mysql_query(conn, qnewCash2);
-                if (qstateTransaction)
-                    cout << "Query failed: " << mysql_error(conn) << "\n";
-            }
-            else cout << "Query failed: " << mysql_error(conn) << "\n";
-
-            //Create cashTransRecoTable if it doesnt exist yet
-            if (tableExists("cashTransRecoTable") == false) {
-                string queryCriarTable = "CREATE TABLE cashTransRecoTable(transactionCount INT KEY AUTO_INCREMENT, senderEmail VARCHAR(255) NOT NULL, receiverEmail VARCHAR(255) NOT NULL, amountSent DOUBLE(5, 2) NOT NULL, feeApplied DOUBLE(5, 2), transDateTime VARCHAR(255) NOT NULL)";
-                const char* qCriarTable = queryCriarTable.c_str();
-                qstateTransaction = mysql_query(conn, qCriarTable);
-                if (qstateTransaction)
-                    cout << "Query failed: " << mysql_error(conn) << "\n";
-            }
-
-            //Inserting values into cashTransRecoTable
-            string amount_toSendString=to_string(amount_toSend), get_transactionFeeString=to_string(get_transactionFee());
-            string queryEmail12amountSent = "INSERT INTO cashTransRecoTable (senderEmail, amountSent, receiverEmail, feeApplied, transDateTime) VALUES ('" + student1.get_email() + "', '" + amount_toSendString + "', '" + receiversEmail + "', '" + get_transactionFeeString + "', '" + getCurrentDateTime() + "')";
-            const char* qEmail12amountSent = queryEmail12amountSent.c_str();
-            qstateTransaction = mysql_query(conn, qEmail12amountSent);
-            if (qstateTransaction) 
-                cout << "Query failed: " << mysql_error(conn) << "\n";
-
-            //Display transaction on the screen
-            cout << "*** Campus Cash (CC)***" << "\n";
-            cout << receiversName << " received $" << fixed << setprecision(2) << amount_toSend << "\n";
+            if (transactionConfirmation == 2)
+                goto send_cashEnd;
         }
     }
     else puts("Connection to DataBase has failed");
+
+send_cashEnd:
+    cout << "";
 }
 //*******
