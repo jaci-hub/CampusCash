4e35add1cb2c1f63a8e638466516cb0e14d4dce6
jaci-hub <jacintoquimua7@gmail.com>
2021-09-03 16:45:07 -0600

    that will be specified in the db

--- maincc/loggedInAsSchoolMenu.h
+++ maincc/loggedInAsSchoolMenu.h
@@ -7,7 +7,7 @@ using namespace std;
 unsigned int menuChoice;
 void schoolOptionMenu() {
     cout << "1- Show Orders\n";
-    cout << "2- Management\n";
+    cout << "2- Management\n"; //show if logged in as manager
     cout << "3- Log Out\n";
     cout << "Please, enter an option: ";
     cin >> menuChoice;
