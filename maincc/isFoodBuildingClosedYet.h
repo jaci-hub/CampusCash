f355ed8159d34c5cf8eb5b62c3b10af31a862d71
jaci-hub <jacintoquimua7@gmail.com>
2021-09-04 14:59:44 -0600

    DONE!!!

--- maincc/isFoodBuildingClosedYet.h
+++ maincc/isFoodBuildingClosedYet.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include "file_com_pragmaonce_mysql.h"
+#include "timeConverter.h"
 
 using namespace std;
 
@@ -17,7 +18,34 @@ bool isFoodBuildingClosedYet(string foodBuildingNameFormated, string aClosingTim
 	conn = mysql_real_connect(conn, "localhost", "root", "ReinoDaMatamba3", "allstudentdata", 3306, NULL, 0);
 
 	if (conn) {
-		return false;
+		//get buildingClosingTime
+		string buildingClosingTime;
+		string queryGettingbuildingClosingTime = "SELECT closingTime FROM " + foodBuildingNameFormated + "WorkingTimesTable";
+		const char* qGettingbuildingClosingTime = queryGettingbuildingClosingTime.c_str();
+		qstateisFoodBuildingClosedYet = mysql_query(conn, qGettingbuildingClosingTime);
+		if (!qstateisFoodBuildingClosedYet) {
+			res = mysql_store_result(conn);
+			row = mysql_fetch_row(res);
+			buildingClosingTime = row[0];
+		}
+		else cout << "Query failed: " << mysql_error(conn) << "\n";
+
+		//take the colon away from buildingClosingTime (now it can be turned into integer)
+		string tempString = timeConverter(buildingClosingTime);
+		int i = tempString.find(':');
+		tempString.erase(i, 1);
+		buildingClosingTime = tempString;
+		
+		//take the colon away from given time (now it can be turned into integer)
+		string givenTime;
+		string tempString1 = timeConverter(aClosingTime);
+		int j = tempString1.find(':');
+		tempString1.erase(j, 1);
+		givenTime = tempString1;
+
+		if (stoi(givenTime) > stoi(buildingClosingTime))
+			return true;
+		else return false;
 	}
 	else puts("Connection to DataBase has failed");
 }
--- maincc/isFoodBuildingOpenYet.h
+++ maincc/isFoodBuildingOpenYet.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include "file_com_pragmaonce_mysql.h"
+#include "timeConverter.h"
 
 using namespace std;
 
@@ -17,7 +18,34 @@ bool isFoodBuildingOpenYet(string foodBuildingNameFormated, string AnOpeningTime
 	conn = mysql_real_connect(conn, "localhost", "root", "ReinoDaMatamba3", "allstudentdata", 3306, NULL, 0);
 
 	if (conn) {
-		return true;
+		//get buildingOpeningTime
+		string buildingOpeningTime;
+		string queryGettingbuildingOpeningTime = "SELECT openingTime FROM " + foodBuildingNameFormated + "WorkingTimesTable";
+		const char* qGettingbuildingOpeningTime = queryGettingbuildingOpeningTime.c_str();
+		qstateisFoodBuildingOpenYet = mysql_query(conn, qGettingbuildingOpeningTime);
+		if (!qstateisFoodBuildingOpenYet) {
+			res = mysql_store_result(conn);
+			row = mysql_fetch_row(res);
+			buildingOpeningTime = row[0];
+		}
+		else cout << "Query failed: " << mysql_error(conn) << "\n";
+
+		//take the colon away from buildingOpeningTime (now it can be turned into integer)
+		string tempString = timeConverter(buildingOpeningTime);
+		int i = tempString.find(':');
+		tempString.erase(i, 1);
+		buildingOpeningTime = tempString;
+
+		//take the colon away from given time (now it can be turned into integer)
+		string givenTime;
+		string tempString1 = timeConverter(AnOpeningTime);
+		int j = tempString1.find(':');
+		tempString1.erase(j, 1);
+		givenTime = tempString1;
+
+		if (stoi(givenTime) >= stoi(buildingOpeningTime))
+			return true;
+		else return false;
 	}
 	else puts("Connection to DataBase has failed");
 }
