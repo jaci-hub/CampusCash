0d0ca5a6b332489887f8fdaebf8db938688d3d1d
jaci-hub <jacintoquimua7@gmail.com>
2021-09-03 18:34:18 -0600

    DONE!!!

--- maincc/criarStaff.h
+++ maincc/criarStaff.h
@@ -20,17 +20,47 @@ void criarStaff() {
         //simulation of log in into the schools platform 
         cout << "*** Campus Cash (CCash) ***" << "\n";
         //ckecking the staff's email
-    loginHere:
-        string staffEmail, staffPassword;
+    tryStaffEmailagain:
+        string check_email;
+        bool check_email_val = false;
         cout << "*** Log in ***" << "\n";
         cout << "e-mail: ";
-        cin >> staffEmail;
-        cout << "password: ";
-        cin >> staffPassword;
-        while (staffEmail != "jquimua@yotes.collegeofidaho.edu" && staffPassword != "ReinoDaMatamba11") {
-            cout << "email or password is incorrect!\n";
-            goto loginHere;
+        cin >> check_email;
+        string queryEmail = "SELECT * FROM staffdatatable";
+        const char* qEmail = queryEmail.c_str();
+        qstateStaff = mysql_query(conn, qEmail);
+        if (!qstateStaff) {
+            res = mysql_store_result(conn);
+            while (row = mysql_fetch_row(res)) {
+                if (check_email == row[1]) {
+                    check_email_val = true;
+                    break;
+                }
+            }
+            if (check_email_val == false) {
+                cout << "email not found!\n";
+                goto tryStaffEmailagain;
+            }
         }
+        else cout << "Query failed: " << mysql_error(conn) << "\n";
+
+        //ckecking the staff's password
+    tryStaffPasswordagain:
+        string check_password;
+        cout << "password: ";
+        cin >> check_password;
+        string queryPassword = "SELECT staffPassword FROM staffdatatable WHERE staffEmail = '" + check_email + "'";
+        const char* qPassword = queryPassword.c_str();
+        qstateStaff = mysql_query(conn, qPassword);
+        if (!qstateStaff) {
+            res = mysql_store_result(conn);
+            row = mysql_fetch_row(res);
+            if (check_password != row[0]) {
+                cout << "password is incorrect!\n";
+                goto tryStaffPasswordagain;
+            }
+        }
+        else cout << "Query failed: " << mysql_error(conn) << "\n";
     }
     else puts("Connection to DataBase has failed");
 }
