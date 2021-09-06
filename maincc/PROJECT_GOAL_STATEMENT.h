f45c08449eedb2404d9160881b939bd459705c4d
jaci-hub <jacintoquimua7@gmail.com>
2021-09-06 13:21:20 -0600

    to review

---
+++ maincc/PROJECT_GOAL_STATEMENT.h
@@ -0,0 +1,13 @@
+#pragma once
+/*
+NAME: JACINTO QUIMUA
+DATE: 1/28/2021
+
+My goal is very simple: to make sure students in all colleges and universities campus feel more entiteled and independent about their "campus cash/food"; I want to provide students more control over their "campus cash", which includes:
+    - enabling students to order food (wheather they are living ON or OFF campus) from the campus cafeteria or any other building that provides food for students - I see a real potential here for more work-study jobs!!!
+    - enabling students to send each other cash.
+    - enabling students to send each other meals.
+
+    The purpose is to allow students to help each other when in need. One example is if a student runs out of meals during the week and is low on cash, then the student can get some cash from a friend and just get food from another food building, so the student don’t have to wait until the meals renew again in the weekend to be able to eat. Another example is if the cafe is closed and a student is low in cash, then he/she can ask a friend to send some cash so he/she can eat in another food building instead.
+    In addition, students DO NOT have a way to check/track how much cash they have left until they go to a certain food building and ask a worker to check it for them, which is usually done by printing it on a paper (and then we have the whole global warming argument coming in!!). So this project also aims to solve that problem. On top of that, to avoid printing receipts in paper(which no one uses anyway, resulting in more paper being wasted again), the receipt can be just sent/reflected directly on the platform instead.
+*/
